a3f326f567c2d14d7f2d2b1985b2ef84a5f68e8d
Thomas Bächler <thomas@archlinux.org>
2014-09-25 00:04:06 +0200

    Move X11 handling into its own file.

--- src/child.c
+++ src/child.c
@@ -4,72 +4,18 @@
 #include "opts.h"
 #include "home.h"
 #include "mounts.h"
-#include "command.h"
 #include "network.h"
 #include "tty.h"
+#include "x11.h"
 #include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
 #include <limits.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-static void get_x11() {
-  char *cmd_argv[8];
-  char *display;
-  int fd;
-
-  if( mkdir(APPJAIL_SWAPDIR "/X11-unix", 0755) == -1 )
-    errExit("mkdir");
-  if( cap_mount("/tmp/.X11-unix", APPJAIL_SWAPDIR "/X11-unix", NULL, MS_BIND, NULL) == -1 )
-    errExit("mount --bind");
-  if( cap_mount(NULL, APPJAIL_SWAPDIR "/X11-unix", NULL, MS_PRIVATE, NULL) == -1 )
-    errExit("mount --make-private");
-
-  display = getenv("DISPLAY");
-  if( display == NULL )
-    return;
-
-  cmd_argv[0] = "xauth";
-  cmd_argv[1] = "-f";
-  cmd_argv[2] = APPJAIL_SWAPDIR "/Xauthority";
-  cmd_argv[3] = "generate";
-  cmd_argv[4] = display;
-  cmd_argv[5] = "MIT-MAGIC-COOKIE-1";
-  cmd_argv[6] = "trusted";
-  cmd_argv[7] = NULL;
-  // Create an empty file to silence an xauth error message
-  if( (fd = open(cmd_argv[2], O_CREAT, 0600)) != -1)
-    close(fd);
-  if( run_command(cmd_argv[0], cmd_argv) != EXIT_SUCCESS )
-    errExit("xauth");
-}
-
-static void setup_x11() {
-  char *home, xauthority[PATH_MAX], *cmd_argv[4];
-
-  if( mkdir("/tmp/.X11-unix", 0755) == -1 )
-    errExit("mkdir");
-  if( cap_mount(APPJAIL_SWAPDIR "/X11-unix", "/tmp/.X11-unix", NULL, MS_MOVE, NULL) == -1 )
-    errExit("mount --move " APPJAIL_SWAPDIR "/X11-unix /tmp/.X11-unix");
-  rmdir("X11-unix");
-
-  home = getenv("HOME");
-  if( home == NULL )
-    return;
-
-  snprintf(xauthority, PATH_MAX-1, "%s/.Xauthority", home);
-  cmd_argv[0] = "mv";
-  cmd_argv[1] = APPJAIL_SWAPDIR "/Xauthority";
-  cmd_argv[2] = xauthority;
-  cmd_argv[3] = NULL;
-  if( run_command(cmd_argv[0], cmd_argv) != EXIT_SUCCESS )
-    errExit("mv");
-}
 
 static void setup_path(const char *name, const char *path, mode_t mode) {
   char p[PATH_MAX];
---
+++ src/x11.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void get_x11();
+void setup_x11();
